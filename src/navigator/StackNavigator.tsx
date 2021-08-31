5495387a3a7f8b3facb999c49511bd1f417aed61
joaquinbian <joacobian21@gmail.com>
2021-08-31 11:23:17 -0300

    adding a stack

---
+++ src/navigator/StackNavigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleAlign: 'center',
+        // headerStyle: {},
+      }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
---
+++ src/screens/PokeFavsScreens.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const PokeFavsScreens = () => {
+  return (
+    <View>
+      <Text>favs pokemons screen</Text>
+    </View>
+  );
+};
+
+export default PokeFavsScreens;
+
+const styles = StyleSheet.create({});
