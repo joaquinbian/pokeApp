c2c95c1fb80558af7aa3cb93192a759c8da58c75
joaquinbian <joacobian21@gmail.com>
2021-09-01 17:27:45 -0300

    pokeDetailScreen almost done

--- src/screens/PokeDetailScreen.tsx
+++ src/screens/PokeDetailScreen.tsx
@@ -21,6 +21,23 @@ const PokeDetailScreen = ({route, navigation}: Props) => {
   const {id, name, picture} = pokemon;
   const {pokemonDetail, isLoading} = usePokemonDetail(id);
 
+  const StatsList = (name: string, stat: number) => {
+    return (
+      <View
+        style={{
+          marginVertical: 2,
+          flexDirection: 'row',
+        }}>
+        <View style={{width: '50%'}}>
+          <Text style={{fontSize: 17}}>{name}</Text>
+        </View>
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 17}}>{stat}</Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <ScrollView style={{marginTop: top + 10, marginHorizontal: 10}}>
       <Text>detail pokemon screen</Text>
@@ -53,6 +70,8 @@ const PokeDetailScreen = ({route, navigation}: Props) => {
       </ScrollView>
       <Text style={styles.title}>Moves</Text>
       <Text>{pokemonDetail?.moves.join(', ')}</Text>
+      <Text style={styles.title}>Stats</Text>
+      {pokemonDetail?.stats.map((s, i) => StatsList(s.stat.name, s.base_stat))}
     </ScrollView>
   );
 };
