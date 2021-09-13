11fe1903dd84ac398b2c46f79a9ab510f9cd57b6
joaquinbian <joacobian21@gmail.com>
2021-09-13 09:18:55 -0300

    some styles in bottom navigator

--- src/navigator/BottomTabNavigator.tsx
+++ src/navigator/BottomTabNavigator.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import StackNavigator from './StackNavigator';
 import SearchScreen from '../screens/SearchScreen';
+import {Platform} from 'react-native';
 
-const TabNavigator = createMaterialBottomTabNavigator();
+const TabNavigator = createBottomTabNavigator();
 const BottomTabNavigator = () => {
   return (
-    <TabNavigator.Navigator>
+    <TabNavigator.Navigator
+      screenOptions={{
+        headerShown: false,
+        //el tabBarLabel son los titulitos del menu
+        tabBarLabelStyle: {
+          marginBottom: Platform.OS === 'android' ? 10 : 0,
+        },
+      }}>
       <TabNavigator.Screen name="home" component={StackNavigator} />
       <TabNavigator.Screen name="search" component={SearchScreen} />
     </TabNavigator.Navigator>
